dbc282175290c3073d55bebcbdce9aacfaf36155
Phong Bui <boushphong@gmail.com>
2022-01-10 18:00:34 +0700

    Mon 10 Jan 2022 06:00:34 PM +07 commit

---
+++ 4) Classes, Constructors and Inheritance/1) Classes/src/academy/learnprogramming/Car.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+public class Car {
+
+    // Change the access modifier from private to public to let a class outside the Car class to access the Car's field.
+    // This is not recommended since it violates the rules of OOP, which is to make the classes' usage restrictive.
+    private int doors;
+    private int wheels;
+    // Private to public
+    public String model;
+    private String engine;
+    private String colour;
+
+}
---
+++ 4) Classes, Constructors and Inheritance/1) Classes/src/academy/learnprogramming/Main.java
@@ -0,0 +1,12 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.model = "Carrera";
+
+        System.out.println(porsche.model);
+    }
+}
