502cf557cb5d00eb2c09fb56ff80bb23b7735d69
Phong Bui <boushphong@gmail.com>
2022-01-12 11:03:43 +0700

    Wed 12 Jan 2022 11:03:43 AM +07 commit

--- 5) Composition, Encapsulation, and Polymorphism/6) Polymorphism Part 2/src/academy/learnprogramming/Main.java
+++ 5) Composition, Encapsulation, and Polymorphism/6) Polymorphism Part 2/src/academy/learnprogramming/Main.java
@@ -116,5 +116,4 @@ public class Main {
         System.out.println(holden.brake());
     }
 
-
 }
---
+++ 6) Arrays, Java inbuilt Lists, Autoboxing and Unboxing/2) Arrays Part 2/src/academy/learnprogramming/Main.java
@@ -0,0 +1,73 @@
+package academy.learnprogramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+	    int[] myIntegers = getIntegers(5);
+
+        int[] sorted = sortedIntegers(myIntegers);
+        printArray(sorted);
+
+        }
+
+
+    public static int[] getIntegers(int capacity) {
+        int[] array = new int[capacity];
+        System.out.println("Enter " + capacity + " integer values:\r");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element " + array[i]);
+        }
+    }
+
+    public static int[] sortedIntegers(int[] array){
+//        int[] sortedArray = new int[array.length];
+//        for (int i=0; i<array.length; i++){
+//            sortedArray[i] = array[i];
+//        }
+
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+
+        boolean flag = true;
+        int temp;
+        while (flag) {
+            flag = false;
+            for (int i=0; i<sortedArray.length-1; i++){
+                if(sortedArray[i] < sortedArray[i+1]){
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true; // Only continues the while loop if we set flag to true.
+                }
+            }
+        }
+        return sortedArray;
+    }
+}
+
+//Python code
+//
+//flag = True
+//
+//while flag:
+//    flag = False
+//    for i in range(len(array) - 1):
+//        if array[i] < array[i+1]:
+//            temp = array[i]
+//            array[i] = array[i+1]
+//            array[i+1] = temp
+//            print(array)
+//            flag = True
+//
+//print(array)
