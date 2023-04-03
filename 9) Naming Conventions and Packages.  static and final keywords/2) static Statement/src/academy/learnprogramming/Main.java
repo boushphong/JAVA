8decada48a3fe290455c1a01901bf833d3cc3656
boushphong <59940078+boushphong@users.noreply.github.com>
2023-04-03 18:59:45 +0700

    Update Main.java

--- 9) Naming Conventions and Packages.  static and final keywords/2) static Statement/src/academy/learnprogramming/Main.java
+++ 9) Naming Conventions and Packages.  static and final keywords/2) static Statement/src/academy/learnprogramming/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-	    StaticTest firstInstance = new StaticTest("1st Instance");
+	StaticTest firstInstance = new StaticTest("1st Instance");
         System.out.println(firstInstance.getName() + " is instance number " + StaticTest.getNumInstances());
 
         StaticTest secondInstance = new StaticTest("2nd instance");
