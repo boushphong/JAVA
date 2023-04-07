ad7eb4d65abaf85ab963b41edcfb4e8de223857f
boushphong <59940078+boushphong@users.noreply.github.com>
2023-04-07 15:22:16 +0700

    Update Main.java

--- 10) Java collections/1) Collections Overview/src/academy/learnprogramming/Main.java
+++ 10) Java collections/1) Collections Overview/src/academy/learnprogramming/Main.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-	    Theatre theatre = new Theatre("Olympian", 8, 12);
+	Theatre theatre = new Theatre("Olympian", 8, 12);
         theatre.getSeats();
         if (theatre.reserveSeat("H11")) {
             System.out.println("Please pay");
